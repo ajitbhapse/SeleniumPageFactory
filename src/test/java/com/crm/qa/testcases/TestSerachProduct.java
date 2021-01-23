6ac892dc7079e3d7401b234018e66eaa81ea1233
ajit bhapse <abhapse@conga.com>
2021-01-23 10:08:06 +0530

    initial commit

---
+++ src/test/java/com/crm/qa/testcases/TestSerachProduct.java
@@ -0,0 +1,47 @@
+package com.crm.qa.testcases;
+
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.pages.ProductSearchPage;
+import com.crm.qa.util.TestUtil;
+import com.crm.qa.util.WebDriverUtils;
+
+public class TestSerachProduct {
+	public WebDriver driver;
+	String filePath = "login";
+	String elementName = "testData";
+	
+	@BeforeMethod
+	public void setUp() {
+		WebDriverUtils utils = new WebDriverUtils();
+		utils.initialization();
+		driver = utils.getDriver();
+	}
+	
+	@Test
+	public void testAddProductToWishList() {
+		Map<String,String> testData = TestUtil.getTestData("TestSerachProduct.json", "testAddProductToWishList");
+		
+		ProductSearchPage productSearchPage = new ProductSearchPage(driver);
+		
+		//enter product to search 
+		productSearchPage.enterProductToBeSearch(testData.get("productName"));
+		
+		//click on search button 
+		productSearchPage.clickOnProductSearch();
+		
+		//click on add to wish list button
+		productSearchPage.addProductToWishList();
+		
+		//get message
+		String msgSuccess = productSearchPage.getText();
+		
+		//verify success message 
+		Assert.assertEquals(msgSuccess, testData.get("successMessage"));
+	}
+}
